35630a256ac31429dd0ce2dde7e37e5baa460461
MONKEYDALYN\a <gogoasealin@gmail.com>
2018-08-18 14:38:20 +0300

    PlayerMovement

---
+++ TimeJourney/TimeJourney/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] CharacterController2D controller;
+
+    public float runSpeed = 20f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+	
+	void Update () {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }else if(Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
