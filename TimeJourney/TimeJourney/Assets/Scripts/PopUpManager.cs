69976c8fbe8a856fd2c2e86c49a038cb263aae80
Gabriel Dinca <33097664+GabrielDinca25@users.noreply.github.com>
2018-09-08 22:59:54 +0300

    Added incomplete pop up on enter area

---
+++ TimeJourney/TimeJourney/Assets/Scripts/PopUpManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PopUpManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("True");
+            gameObject.SetActive(false);
+        }
+    }
+  
+}
