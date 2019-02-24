fd0cffa118f2345be4d273677900a247d063a08f
gogoasealin <gogoasealin@github.com>
2019-02-24 10:57:38 +0200

    Reworked shotMovement going how long they are visible TextBox sorting layer is now player and have -2/-1 so the player still be render on top.

---
+++ TimeJourney/TimeJourney/Assets/Scripts/Player/Weapons/Shots/ShotMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotMovement : MonoBehaviour {
+
+    public Vector3 target;
+    public float speed = 1;
+    private Vector3 destination;
+
+    private void OnEnable()
+    {
+        destination = (target - transform.position) * 100; 
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, destination, Time.deltaTime * speed);
+        if(transform.position == target)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+
+    private void OnBecameInvisible()
+    {
+        gameObject.SetActive(false);
+    }
+}
