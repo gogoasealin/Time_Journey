42d00e1c456af60fd63617cd54977b49af2526a7
gogoasealin <gogoasealin@github.com>
2019-03-24 14:20:30 +0200

    Added open door with password

---
+++ TimeJourney/TimeJourney/Assets/Scripts/SpecialScripts/OpenDoreWithPassword/ActivateByTrigger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ActivateByTrigger : MonoBehaviour
+{
+    public GameObject m_SwordLogic;
+    public GameObject m_Canvas;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            m_SwordLogic.SetActive(false);
+            m_Canvas.SetActive(false);
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Disable();
+        }
+    }
+
+    public void Disable()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+        m_SwordLogic.SetActive(true);
+        m_Canvas.SetActive(true);
+    }
+}
---
+++ TimeJourney/TimeJourney/Assets/Scripts/SpecialScripts/OpenDoreWithPassword/TypePassword.cs
@@ -0,0 +1,57 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TypePassword : MonoBehaviour
+{
+    private string m_CurrentSentence;
+    private int m_CurrentLetter;
+
+    public TMP_Text m_CurrentSentenceText;
+    public string password;
+    //public GameObject Door;
+
+    private void Start()
+    {
+        ResetText();
+    }
+    private void SetText()
+    {
+        m_CurrentSentenceText.text = m_CurrentSentence;
+    }
+
+    private void ResetText()
+    {
+        m_CurrentLetter = 0;
+        m_CurrentSentence = "____";
+        SetText();
+    }
+
+    public void SetNextCharacter(string letter)
+    {  
+        char[] ch = m_CurrentSentence.ToCharArray();
+        ch[m_CurrentLetter] = letter.ToCharArray()[0];
+        m_CurrentSentence = new string(ch);
+        SetText();
+
+        if (m_CurrentLetter < m_CurrentSentence.Length - 1)
+        {
+            m_CurrentLetter++;
+        }
+    }
+
+    public void CheckValidation()
+    {
+        if (m_CurrentSentence.Equals(password))
+        {
+            Debug.Log("open door");
+            GetComponent<ActivateByTrigger>().Disable();
+            Destroy(GetComponent<ActivateByTrigger>());
+            GetComponent<TypePassword>().enabled = false;
+            //Door.GetComponent<ActivateDoor>().enabled = true;
+        }
+        else
+        {
+            ResetText();
+        }
+    }
+}
