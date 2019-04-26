54341060094f479cdc434f7b7f54b8de030676fd
gogoasealin <gogoasealin@github.com>
2019-04-26 10:17:19 +0300

    Fixed SimplePlayer movement

--- TimeJourney/TimeJourney/Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ TimeJourney/TimeJourney/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -4,35 +4,51 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public CharacterController2D controller;
     public Animator animator;
+    public Rigidbody2D m_Rigidbody2D;
 
-    public float runSpeed = 7f;
+    public float runSpeed = 30f;
+    public bool m_FacingRight;  // For determining which way the player is currently facing.
 
     float horizontalMove = 0f;
-    bool jump = false;
-	
-	void Update () {
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
+    private Vector3 m_Velocity = Vector3.zero;
+
+    void Update () {
 
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-        if(Input.GetButtonDown("Jump"))
-        {
-            jump = true;
-            animator.SetBool("IsJumping", true);
-        }
     }
 
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
-        jump = false;
+        Move(horizontalMove * Time.fixedDeltaTime);
     }
 
-    public void OnLanding()
+    public void Move(float move)
     {
-        animator.SetBool("IsJumping", false);
+        Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+
+        m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+        if (move > 0 && !m_FacingRight)
+        {
+            Flip(0.3f);
+        }
+        else if (move < 0 && m_FacingRight)
+        {
+            Flip(-0.3f);
+        }
     }
+
+    private void Flip(float moveDistantion)
+    {
+        m_FacingRight = !m_FacingRight;
+        transform.Rotate(0f, 180f, 0f);
+        transform.position += new Vector3(moveDistantion, 0, 0);
+
+    }
+
 }
