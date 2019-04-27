59ba7b11a2394cfcb66cd970fc0b8c5f023cf01f
GabrielDinca25 <gdincagabriel@gmail.com>
2019-04-27 16:47:55 +0300

    Added woodlog blocked water that kills enemies

---
+++ TimeJourney/TimeJourney/Assets/WoodlogHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodlogHealth : Health
+{
+    public GameObject watermapBlocked;
+    public GameObject watermapFree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void GetDamage(int dmgAmount)
+    {
+        watermapBlocked.SetActive(false);
+        watermapFree.SetActive(true);
+        Destroy(gameObject);
+    }
+}
