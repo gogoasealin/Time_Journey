e72819b8f09acd00df4d32dfebc919d73afed172
GabrielDinca25 <gdincagabriel@gmail.com>
2019-06-22 18:33:48 +0300

    Added stairs and spikes for slippery level

---
+++ TimeJourney/TimeJourney/Assets/IceStairs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceStairs : MonoBehaviour
+{
+    public GameObject stairsPlatform;
+    public GameObject smoothPlatform;
+
+    public float startDelay;
+    public float frequency;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("ChangeStairs", startDelay, frequency);
+    }
+
+    void ChangeStairs()
+    {
+        stairsPlatform.SetActive(!stairsPlatform.activeSelf);
+        smoothPlatform.SetActive(!smoothPlatform.activeSelf);
+    }
+}
