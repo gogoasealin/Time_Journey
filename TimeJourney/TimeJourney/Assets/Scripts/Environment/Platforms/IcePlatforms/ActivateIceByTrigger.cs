39a7fb34161c5cf24ccd549c774691b9d08a6a50
gogoasealin <gogoasealin@github.com>
2019-06-22 16:19:36 +0300

    Added IceEffect for platforms

---
+++ TimeJourney/TimeJourney/Assets/Scripts/Environment/Platforms/IcePlatforms/ActivateIceByTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ActivateIceByTrigger : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+}
---
+++ TimeJourney/TimeJourney/Assets/Scripts/Environment/Platforms/IcePlatforms/IceEffect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class IceEffect : MonoBehaviour
+{
+    public float force;
+    private AreaEffector2D ae2D;
+
+    private void Start()
+    {
+        ae2D = GetComponent<AreaEffector2D>();
+    }
+
+    private void Update()
+    {
+        bool right = GameController.instance.player.GetComponent<CharacterController2D>().m_FacingRight;
+
+        if (right)
+        {
+            ae2D.forceMagnitude = force;
+        }
+        else
+        {
+            ae2D.forceMagnitude = -force;
+        }
+    }
+}
