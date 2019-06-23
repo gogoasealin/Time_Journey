3514c586340eda202c71c7167eb87e0bb8ca3146
gogoasealin <gogoasealin@github.com>
2019-06-23 11:36:16 +0300

    Fixed minor issues Level3Easy start working at EvilWizard

---
+++ TimeJourney/TimeJourney/Assets/Scripts/Bosses/Level4EvilWizard/EvilWizardHealth.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EvilWizardHealth : Health
+{
+    private IEnumerator damageAnimation;
+    public SpriteRenderer[] bodyParts;
+    public TriggerBossFight triggerBossFight;
+
+    private void OnEnable()
+    {
+        transform.localScale = new Vector3(0.003f, 0.003f, 0);
+        m_CurrentHealth = m_maxHp;
+        GetComponent<EvilWizardMovementStage1>().enabled = true;
+    }
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void GetDamage(int dmgAmount)
+    {
+        m_CurrentHealth -= dmgAmount;
+        if (m_CurrentHealth <= 0)
+        {
+            Die();
+            return;
+        }
+        GetDamageAnimation();
+    }
+
+    public override void GetDamage(string type, int dmgAmount)
+    {
+        m_CurrentHealth -= dmgAmount;
+        if (m_CurrentHealth <= 0)
+        {
+            Die();
+            return;
+        }
+        GetDamageAnimation();
+    }
+
+
+    public override void GetDamageAnimation()
+    {
+        damageAnimation = DamageAnimation();
+        PrepareNextDamageAnimation();
+        StartCoroutine(damageAnimation);
+    }
+    public void PrepareNextDamageAnimation()
+    {
+        StopAllCoroutines();
+        for (int j = 0; j < bodyParts.Length; j++)
+        {
+            bodyParts[j].enabled = true;
+        }
+    }
+
+    public IEnumerator DamageAnimation()
+    {
+        for (int i = 0; i < 12; i++)
+        {
+            for (int j = 0; j < bodyParts.Length; j++)
+            {
+                bodyParts[j].enabled = !bodyParts[j].enabled;
+            }
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
+    public override void Die()
+    {
+        triggerBossFight.Revert();
+    }
+
+}
